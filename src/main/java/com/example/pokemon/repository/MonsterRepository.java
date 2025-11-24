8813f0bff234cfc80abcc120d817bb5bbdaf2d0e
munpian <ayumu.t.n.21@gmail.com>
2025-11-24 20:31:59 +0900

    ディレクトリ構成

--- src/main/java/com/example/pokemon/repository/MonsterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.pokemon.repository;
-
-import com.example.pokemon.entity.Monster;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MonsterRepository extends JpaRepository<Monster, Long> {
-}
